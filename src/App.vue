4643858ac00e1ec7aefbb2936842170d5f5ed645
Tom <tom.rix@munol.org>
2020-10-05 21:44:56 +0200

    Fix link to repo

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 
       <v-spacer></v-spacer>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/TomTomRixRix/Feedbackbuch"
         target="_blank"
         text
       >
