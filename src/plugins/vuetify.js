0762580a7ca982b950c1dac0375246d0fd730a27
Tom <tom.rix@munol.org>
2020-10-05 21:33:35 +0200

    Added vuetify color theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#c71826',
+            secondary: '#b0bec5',
+            accent: '#f4f1ea',
+            error: '#b71c1c',
+          },
+        },
+      },
 });
