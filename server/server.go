a11a4cf89d9c2482d58be824a7c22e5da92b68cc
Christian Heusel <christian@heusel.eu>
2020-10-06 14:16:26 +0200

    remove unnecessary debug stuff
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- server/server.go
+++ server/server.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/TomTomRixRix/Feedbackbuch/server/graph"
 	"github.com/TomTomRixRix/Feedbackbuch/server/graph/generated"
 	"github.com/TomTomRixRix/Feedbackbuch/server/graph/model"
@@ -65,15 +64,10 @@ func main() {
 	srv.AddTransport(transport.MultipartForm{})
 
 	router.Handle("/query", srv)
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		panic(err)
 	}
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
